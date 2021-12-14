84fadc9d8519e22fc369090a4503ac4240bbb949
Rodrigo <rodmtl@outlook.es>
2021-12-13 21:21:44 -0500

    Initiel commit
    Folder structure And basic menu

---
+++ Demo.RPS/Program.cs
@@ -0,0 +1,65 @@
+﻿using Demo.RPS.Services;
+using System;
+
+namespace Demo.RPS
+{
+    /// <summary>
+    /// Rock paper scissors - console game
+    /// Rodrigo Riveros - 2021
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                GamePlay();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\t\tOoops. something went wrong.");
+                Console.WriteLine($"\t\tDetails\n{ex}");
+                throw;
+            }
+        }
+
+        private static void GamePlay()
+        {
+
+            int gameplayOption = UIGenerator.GetMenuOption();
+            if (gameplayOption == 1)
+            {
+                Console.Clear();
+                Console.WriteLine("\t\tSingle Player Mode");
+            }
+            else if (gameplayOption == 2)
+            {
+                Console.Clear();
+                Console.WriteLine("\t\tMulti Player Mode");
+            }
+            else if(gameplayOption == 3)
+            {
+                Console.Clear();
+                Console.WriteLine("\t\tAdd Weapons");
+            }
+            else if (gameplayOption == 3)
+            {
+                Console.Clear();
+                Console.WriteLine("\t\tEnable Random ComputerMode");
+            }
+
+
+        }
+        private int Battle()
+        {
+            int round = 1;
+            int winner = 0;
+            do
+            {
+
+                round++;
+            } while (winner == 0 && round < 4);
+            return winner;
+        }
+    }
+}
---
+++ Demo.RPS/Services/UIGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.RPS.Services
+{
+    public static class UIGenerator
+    {
+        public static int GetMenuOption()
+        {
+            int menuOption = int.MaxValue;
+            do
+            {
+                //Game Header
+                Console.Clear();
+                Console.WriteLine("\t----------------------------------");
+                Console.WriteLine("\t----- Rock Paper Scissors --------");
+                Console.WriteLine("\t----------------------------------");
+                //Menu
+                Console.WriteLine("\t\t1. Single Player");
+                Console.WriteLine("\t\t2. Multiplayer");
+                Console.WriteLine("\t\t3. Add Weapons");
+                Console.WriteLine("\t\t4. Enable Computer Random Play");
+                Console.WriteLine("\n\n\tExit with 0");
+                string option = Console.ReadLine();
+                Console.WriteLine($"Option Selected: {option}");
+                if (!int.TryParse(option, out menuOption) || menuOption < 0)
+                {
+                    menuOption = int.MaxValue;
+                }
+
+            } while (menuOption == int.MaxValue);
+            return menuOption;
+        }
+    }
+}
